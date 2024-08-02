f8f32cd088ecdbf50e695b5df9db539a1bbed852
Each Chen <iach526526@gmail.com>
2024-08-02 11:47:17 +0800

    plugin

---
+++ extension/README.md
@@ -0,0 +1,2 @@
+## 竄改猴
+這個資料夾的程式會在 Tampermonkey 上執行，連結本地 local host 的 flask
--- plugin.py
+++ plugin.py
@@ -6,13 +6,15 @@ app = Flask(__name__)
 CORS(app)
 api_KEY = os.getenv('HUNTER_IO_API_KEY')
 
+counter = 0
 @app.route('/check_email', methods=['POST'])
 def check_email():
+    global counter#for debug
     email = request.json.get('email')
     if not email:
         return jsonify({'error': 'No email provided'}), 400
     res=mailStatus(email)
-    print(".route('/check_email')",res)
+    print("/check_email  -------------",counter);counter+=1
     return res
     
 
